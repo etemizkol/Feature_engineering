4194493711d68d29b03e8844cd04a065e8dff68c
etemizkol <emirhan.temizkol@gmail.com>
2021-02-21 01:41:46 +0300

    firs commit

---
+++ feature_engineering.py
@@ -0,0 +1,28 @@
+import numpy as pd
+import pandas as pd
+import math
+import statsmodels.stats.api as sms
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', 10)
+pd.set_option('display.float_format', lambda x: '%.5f' % x)
+pd.set_option('display.expand_frame_repr', False)
+
+# titanic, hitters,diabetes verisetleri üzerinde feature engineering çalışmalarını yürütünüz
+
+# Feature Engineering - Data Pre-Processing
+# kem küm
+
+
+
+
+
+
+
+
+#------------------------------------------------Titanic-----------------------------------------------------------#
+
+
+df_titanic = pd.read_csv(r"D:\DATA SCIENCE\VAHİT BAŞKAN\7. Hafta\odev_1_feature_engineering\titanic.csv")
+
+
